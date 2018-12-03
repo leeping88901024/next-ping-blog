a1dbfc96b1163f45c595712dd817897a1b9078ca
lee <leeping88901024@gmail.com>
2018-12-03 09:15:29 +0800

    Update env.config.js

--- env.config.js
+++ env.config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    'process.env.BACKEND_URL': prod ? 'http://localhost:80' : 'http://localhost:80'
+    'process.env.BACKEND_URL': prod ? 'http://www.macdull.top' : 'http://localhost:80'
 }   
