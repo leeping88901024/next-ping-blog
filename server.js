059f1b5129fa98d35cb21148b5d1797ab72c03cf
lee <leeping88901024@gmail.com>
2018-12-05 12:14:44 +0800

    add log

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express')
+var fs = require('fs')
+var path = require('path')
+var rfs = require('rotating-file-stream')
 var next = require('next')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
@@ -16,9 +19,21 @@ var blogRouter = require('./routes/blog')
 app.prepare()
   .then(() => {
     var server = express()
+    var logDirectory = path.join(__dirname, 'log')
+
+    // ensure log directory exists
+    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+    // create a rotating write stream
+    var accessLogStream = rfs('access.log', {
+      interval: '1d',  // rotate daily
+      path: logDirectory
+    })
 
     // middleware
-    server.use(logger('dev'))
+
+    // setup the logger
+    server.use(logger('common', { stream: accessLogStream }))
     server.use(bodyParser.json({ limit: '100mb', extended: true }))
     server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
     server.use(cookieParser())
