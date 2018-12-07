3322e7d407118607f2063c18b7b2498bf3326ae9
lee <leeping88901024@gmail.com>
2018-12-07 10:21:57 +0800

    fix db conn

--- routes/blog.js
+++ routes/blog.js
@@ -8,12 +8,12 @@ var { dbstring } = require('../config')
 // read blogs
 router.get('/blogs',(req,res) => {
     // res.send('there is blog post')
-    MongoClient.connect(dbstring.uri, { useNewUrlParser: true }, (err, client) => {
+    MongoClient.connect(dbstring.uri, { useNewUrlParser: true }, (err, conn) => {
         if (err) {
             console.log('An error occurred connecting to MongoDB:', err)
             return
         }
-        var blogCollection = client.db('Blog').collection('blog')
+        var blogCollection = conn.db('Blog').collection('blog')
         blogCollection.find({}).sort({ createDate: -1 }).toArray((err, docs) => {
             if (err) {
                 console.log(err)
@@ -21,18 +21,19 @@ router.get('/blogs',(req,res) => {
             }
             res.send(docs)
         })
+        conn.close()
     })
 })
 
 // read a blog
 router.get('/blog', (req, res) => {
     var _id = objectId(req.query._id)
-    MongoClient.connect(dbstring.uri, { useNewUrlParser: true }, (err, client) => {
+    MongoClient.connect(dbstring.uri, { useNewUrlParser: true }, (err, conn) => {
         if (err) {
             console.log('An error occurred connecting to MongoDB:', err)
             return
         }
-        var blogCollection = client.db('Blog').collection('blog')
+        var blogCollection = conn.db('Blog').collection('blog')
         blogCollection.findOne({ _id: _id }, (err, doc) => {
             if (err) {
                 console.log(err)
@@ -40,7 +41,7 @@ router.get('/blog', (req, res) => {
             }
             res.send(doc)
         })
-
+        conn.close()
     })
 })
 
@@ -52,12 +53,12 @@ router.post('/create', (req, res) => {
         ...post,
         createDate: new Date()
     }
-    MongoClient.connect(dbstring.uri, { useNewUrlParser: true }, (err, client) => {
+    MongoClient.connect(dbstring.uri, { useNewUrlParser: true }, (err, conn) => {
         if (err) {
             console.log('An error occurred connecting to MongoDB:', err)
             return
         }
-        var blogCollection = client.db('Blog').collection('blog')
+        var blogCollection = conn.db('Blog').collection('blog')
         blogCollection.insertOne(blog)
         .then(result => {
             console.log(`the insertedCount is ${result.insertedCount}`)
@@ -65,7 +66,7 @@ router.post('/create', (req, res) => {
                 insertedCount: result.insertedCount
             })
         });
-        client.close()
+        conn.close()
     })
 })
 
